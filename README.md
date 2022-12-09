5a9461262d6a158114ab20deae461349cac93c80
mmckeever23 <53344038+mmckeever23@users.noreply.github.com>
2022-12-09 14:37:33 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Angular-Tour-of-Heroes
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-k2ix86)
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  /*
+  hero = 'Windstorm';
+  */
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
